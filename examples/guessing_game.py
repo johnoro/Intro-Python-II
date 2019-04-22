7df6d57763dabc1247b11d87bac808413c77552d
johnoro <officialjohnorourke@gmail.com>
2019-04-22 16:47:58 -0400

    Cleans up guessing game slightly

--- examples/guessing_game.py
+++ examples/guessing_game.py
@@ -1,28 +1,26 @@
-import random
+from random import randrange
 
 def guessing_game():
-  print("Guess the number!")
+  print('Guess the number!')
 
-  secret_number = random.randrange(101)
+  secret_number = randrange(101)
 
   while True:
-    guess = input("Input your guess: ")
-    
+    guess = input('Input your guess: ')
     try:
       guess = int(guess)
     except ValueError:
-      print("Please enter an integer.")
+      print('Please enter an integer.')
       continue
 
-    print(f"You guessed: {guess}")
-
+    print(f'You guessed: {guess}')
     if guess == secret_number:
-      print("You win!")
+      print('You win!')
       break
     elif guess < secret_number:
-      print("Too small!")
+      print('Too small!')
     else:
-      print("Too big!")
+      print('Too big!')
 
 if __name__ == '__main__':
   guessing_game()
